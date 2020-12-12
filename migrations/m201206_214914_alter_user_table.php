f9b6e5fdf48ee967928d912a4ebbf067708812b1
inf132221 <malgorzata.frackowiak@student.put.poznan.pl>
2020-12-12 10:42:08 +0100

    convert 'y' to 1 done

--- migrations/m201206_214914_alter_user_table.php
+++ migrations/m201206_214914_alter_user_table.php
@@ -10,14 +10,56 @@ class m201206_214914_alter_user_table extends Migration
 
     public function safeUp()
     {
+        $true_root = (new \yii\db\Query())
+            ->select(['user_id'])
+            ->from('user')
+            ->where(['user_root' => 'y'])
+            ->all();
+
+        $true_active = (new \yii\db\Query())
+            ->select(['user_id'])
+            ->from('user')
+            ->where(['user_active' => 'y'])
+            ->all();
+
         $this->alterColumn('user', 'user_active', $this->tinyInteger(1)->notNull());
         $this->alterColumn('user', 'user_root', $this->tinyInteger(1)->notNull());
+        $this->update('user', ['user_root' => 1], ['user_id' => $true_root]);
+        $this->update('user', ['user_active' => 1], ['user_id' => $true_active]);
     }
 
     public function safeDown()
     {
+        $true_root = (new \yii\db\Query())
+            ->select(['user_id'])
+            ->from('user')
+            ->where(['user_root' => 1])
+            ->all();
+
+        $true_active = (new \yii\db\Query())
+            ->select(['user_id'])
+            ->from('user')
+            ->where(['user_active' => 1])
+            ->all();
+
+        $false_root = (new \yii\db\Query())
+            ->select(['user_id'])
+            ->from('user')
+            ->where(['user_root' => 0])
+            ->all();
+
+        $false_active = (new \yii\db\Query())
+            ->select(['user_id'])
+            ->from('user')
+            ->where(['user_active' => 0])
+            ->all();
+
         $this->alterColumn('user', 'user_active', 'CHAR(1) NOT NULL');
         $this->alterColumn('user', 'user_root', 'CHAR(1) NOT NULL');
+        $this->update('user', ['user_root' => 'y'], ['user_id' => $true_root]);
+        $this->update('user', ['user_active' => 'y'], ['user_id' => $true_active]);
+        $this->update('user', ['user_root' => 'n'], ['user_id' => $false_root]);
+        $this->update('user', ['user_active' => 'n'], ['user_id' => $false_active]);
 
         return false;
     }
--- migrations/m201211_180058_alter_page_table.php
+++ migrations/m201211_180058_alter_page_table.php
@@ -9,13 +9,33 @@ class m201211_180058_alter_page_table extends Migration
 {
     public function safeUp()
     {
+        $true_column = (new \yii\db\Query())
+            ->select(['page_id'])
+            ->from('page')
+            ->where(['page_main' => 'y'])
+            ->all();
 
         $this->alterColumn('page', 'page_main', $this->tinyInteger(1)->notNull());
+        $this->update('page', ['page_main' => 1], ['page_id' => $true_column]);
     }
 
     public function safeDown()
     {
+        $true_column = (new \yii\db\Query())
+            ->select(['page_id'])
+            ->from('page')
+            ->where(['page_main' => 1])
+            ->all();
+
+        $false_column = (new \yii\db\Query())
+            ->select(['page_id'])
+            ->from('page')
+            ->where(['page_main' => 0])
+            ->all();
+
         $this->alterColumn('page', 'page_main', 'CHAR(1) NOT NULL');
+        $this->update('page', ['page_main' => 'y'], ['page_id' => $true_column]);
+        $this->update('page', ['page_main' => 'n'], ['page_id' => $false_column]);
 
     }
 
